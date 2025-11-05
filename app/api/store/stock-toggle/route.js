bd975955e041003cdf6199d287106dff18be6213
Phil Belinsky <philbelinsky@yahoo.com>
2025-11-05 08:49:18 -0500

    fixed import paths

--- app/api/store/stock-toggle/route.js
+++ app/api/store/stock-toggle/route.js
@@ -1,6 +1,6 @@
 import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import { authSeller } from "../middlewares/authSeller";
+import { authSeller } from "../../../middlewares/authSeller";
 import prisma from "@/lib/prisma";
 
 export async function POST(request) {
